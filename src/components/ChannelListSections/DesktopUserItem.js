6a52c1c5e172a6c194b467d5e7b0bb8b8457bba9
yaokaoya <mahoagray@gmail.com>
2022-01-16 22:42:58 +0900

    finished

--- src/components/ChannelListSections/DesktopUserItem.js
+++ src/components/ChannelListSections/DesktopUserItem.js
@@ -4,12 +4,15 @@ import Avatar from '@material-ui/core/Avatar';
 import { Button, Grid, IconButton, Link, Typography } from '@material-ui/core';
 import { NotificationsNoneOutlined } from '@material-ui/icons';
 import Tooltip from '@material-ui/core/Tooltip';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import { useDispatch, useSelector } from "react-redux";
+import Divider from '@material-ui/core/Divider';
+
+import Popup from '../HomeSections/Desktop/Popup';
 
 import { toggleSubscribe } from "../../slices/videoSlice";
 import { addSubscribedUsers } from "../../slices/authSlice";
@@ -51,6 +54,14 @@ const useStyles = makeStyles((theme) => ({
         padding: theme.spacing(1.5, 0.5),
         justifyContent: 'space-around',
     },
+    title: {
+        fontWeight: 400,
+        fontSize: "1.1rem",
+        padding: theme.spacing(2, 3, 0, 3),
+    },
+    contentText: {
+        margin: 0,
+    },
 }));
 
 export default function DesktopUserItem({ user }) {
@@ -59,10 +70,14 @@ export default function DesktopUserItem({ user }) {
     const { _id, avatar, displayName, subscribersCount, videosCount, description } = user.userTo;
     const letterAvatar = displayName.charAt(0).toUpperCase();
     const dispatch = useDispatch();
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const location = useLocation();
 
     const auth = useSelector((state) => state.auth);
     const isMe = auth?.user?._id === _id
     const isSubscribed = auth?.user?.subscribedUsers?.some((user) => user.userTo._id === _id)
+    const isAuthenticated = auth.isAuthenticated;
 
     const handleOpenModal = () => {
         setOpenModal(true);
@@ -72,14 +87,22 @@ export default function DesktopUserItem({ user }) {
         setOpenModal(false);
     };
 
-    const handleSubscribe = () => {
-        dispatch(toggleSubscribe(_id));
-        dispatch(addSubscribedUsers(user))
+    const handleSubscribe = (event) => {
+        if (isAuthenticated) {
+            dispatch(toggleSubscribe(_id));
+            dispatch(addSubscribedUsers(user))
+        } else {
+            setAnchorEl(event.currentTarget);
+        }
     };
 
     const handleUnsubscribe = () => {
         dispatch(toggleSubscribe(_id));
         setOpenModal(false);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
     };
 
     return (
@@ -147,6 +170,30 @@ export default function DesktopUserItem({ user }) {
                     </DialogActions>
                 </Dialog>
             )}
+
+            <Popup open={Boolean(anchorEl)}
+                anchorEl={anchorEl}
+                handleClose={handleClose}
+            >
+                <Typography className={classes.title}>
+                    Want to subscribe to this channel?
+                </Typography>
+                <DialogContent>
+                    <DialogContentText className={classes.contentText} variant="body2">
+                        Sign in to subscribe to this channel.
+                    </DialogContentText>
+                </DialogContent>
+                <Divider />
+                <DialogActions >
+                    <Button
+                        component={NavLink}
+                        to={{ pathname: "/login", state: { from: location.pathname } }}
+                        color="primary"
+                    >
+                        Sign in
+                    </Button>
+                </DialogActions>
+            </Popup>
 
         </>
     )
